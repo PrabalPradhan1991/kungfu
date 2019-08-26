39032bc405bc10c07b48f99b8ac744b014355f2a
johnstha <uniweb.johnstha@gmail.com>
2019-08-26 13:23:31 +0545

    changes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,6 +2,17 @@
 @include('user.include.header')
 
 <div class="container">
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-6">
+			<div class="page-not-found">
+				<h1><i class="fas fa-bug"></i> 404</h1>
+				<h2>Page not found</h2>
+				<p>We're sorry, the page you requested could not found.<br>Please contact us at <a href="mailto:jahungchi@bigpond.com">jahungchi@bigpond.com</a> </p>
+			</h1>
+		</div>
+		<div class="col-md-3"></div>
+	</div>
 	
 </div>
 @include('user.include.footer')
